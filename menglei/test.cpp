cda7215f981099d73e4b235ea39f4f074fe1fc84
lymenglei <ly-menglei@163.com>
2018-06-15 18:25:00 +0800

    add menglei dir

---
+++ menglei/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <xlnt/xlnt.hpp>
+
+//using namespace std;
+
+int main()
+{
+    // TODO write your codes here
+	xlnt::workbook wb;
+    xlnt::worksheet ws = wb.active_sheet();
+    ws.cell("A1").value(5);
+    ws.cell("B2").value("string data");
+    ws.cell("C3").formula("=RAND()");
+    ws.merge_cells("C3:C4");
+    ws.freeze_panes("B2");
+    wb.save("example.xlsx");
+
+
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
